9e136ccdf7ccb05d9dfd5b64a93f4d4410423ca1
Max de Visser <m.madv96@gmail.com>
2023-04-24 20:57:34 +0200

    progress

--- src/max.py
+++ src/max.py
@@ -1,21 +1,42 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from PIL import Image
 
 
 def locate_data(
-    path: Path,
+    data_path: Path,
+    show: bool = False,
 ) -> tuple[pd.DataFrame, list, list, list, list, list, list]:
     """Returns the data as ordered paths, so we can load the images when we need them"""
-    class_lables = pd.read_csv(path / "class_dict.csv")
-    X_train = sorted([x for x in path.glob("train/*")])
-    y_train = sorted([x for x in path.glob("train_labels/*")])
-    X_test = sorted([x for x in path.glob("test/*")])
-    y_test = sorted([x for x in path.glob("test_labels/*")])
-    X_val = sorted([x for x in path.glob("val/*")])
-    y_val = sorted([x for x in path.glob("val_labels/*")])
+    class_lables = pd.read_csv(data_path / "class_dict.csv").set_index("name")
+    X_train = sorted([x for x in data_path.glob("train/*")])
+    y_train = sorted([x for x in data_path.glob("train_labels/*")])
+    X_test = sorted([x for x in data_path.glob("test/*")])
+    y_test = sorted([x for x in data_path.glob("test_labels/*")])
+    X_val = sorted([x for x in data_path.glob("val/*")])
+    y_val = sorted([x for x in data_path.glob("val_labels/*")])
+
+    if show:
+        data_counts = [X_train, y_train, X_test, y_test, X_val, y_val]
+        n = len(data_counts)
+        multiplier = 0
+        w = 0.5
+        x = np.arange(n)  # x positions
+
+        _, ax = plt.subplots(figsize=(6, 5), layout="constrained")
+        for i in range(n):
+            value = len(data_counts[i])
+            rect = ax.bar(
+                x=x[i], height=value, width=w
+            )
+            ax.bar_label(rect, padding=3)
+            #ax.bar(x=labels, height=[len(y_train), len(y_test), len(y_val)], width=w)
+            ax.set_title("Data Distribution")
+        labels = ["Train", "Test", "Validation"]
+        #plt.xticks(x, labels)
 
     return (
         class_lables,
