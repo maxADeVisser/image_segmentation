bcc25a0178870c72829be3ae7f8257e23aa4e538
Max de Visser <m.madv96@gmail.com>
2023-04-26 20:13:04 +0200

    cleanup

---
+++ src/_types.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class RGB:
+    r: int
+    g: int
+    b: int
+
+    def __str__(self) -> str:
+        return f"RGB({self.r}, {self.g}, {self.b})"
+
+    def get_label(self) -> str:
+        """Return the label that matches the RGB values in CLASS_COLOR_LABELS"""
+        for key, value in CLASS_COLOR_LABELS.items():
+            if value == self:
+                return key
+        else:
+            raise ValueError(f"No label exists for RGB-value {self}")
+
+
+# RGB values for each class label in the dataset
+CLASS_COLOR_LABELS = {
+    "Animal": RGB(64, 128, 64),
+    "Archway": RGB(192, 0, 128),
+    "Bicyclist": RGB(0, 128, 192),
+    "Bridge": RGB(0, 128, 64),
+    "Building": RGB(128, 0, 0),
+    "Car": RGB(64, 0, 128),
+    "CartLuggagePram": RGB(64, 0, 192),
+    "Child": RGB(192, 128, 64),
+    "Column_Pole": RGB(192, 192, 128),
+    "Fence": RGB(64, 64, 128),
+    "LaneMkgsDriv": RGB(128, 0, 192),
+    "LaneMkgsNonDriv": RGB(192, 0, 64),
+    "Misc_Text": RGB(128, 128, 64),
+    "MotorcycleScooter": RGB(192, 0, 192),
+    "OtherMoving": RGB(128, 64, 64),
+    "ParkingBlock": RGB(64, 192, 128),
+    "Pedestrian": RGB(64, 64, 0),
+    "Road": RGB(128, 64, 128),
+    "RoadShoulder": RGB(128, 128, 192),
+    "Sidewalk": RGB(0, 0, 192),
+    "SignSymbol": RGB(192, 128, 128),
+    "Sky": RGB(128, 128, 128),
+    "SUVPickupTruck": RGB(64, 128, 192),
+    "TrafficCone": RGB(0, 0, 64),
+    "TrafficLight": RGB(0, 64, 64),
+    "Train": RGB(192, 64, 128),
+    "Tree": RGB(128, 128, 0),
+    "Truck_Bus": RGB(192, 128, 192),
+    "Tunnel": RGB(64, 0, 64),
+    "VegetationMisc": RGB(192, 192, 0),
+    "Void": RGB(0, 0, 0),
+    "Wall": RGB(64, 192, 0),
+}
--- src/max.py
+++ src/max.py
@@ -1,17 +1,20 @@
+import pickle
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from PIL import Image
+from tqdm import tqdm
+
+from _types import CLASS_COLOR_LABELS, RGB
 
 
 def locate_data(
     data_path: Path,
     show: bool = False,
-) -> tuple[pd.DataFrame, list, list, list, list, list, list]:
-    """Returns the data as ordered paths, so we can load the images when we need them"""
-    class_lables = pd.read_csv(data_path / "class_dict.csv").set_index("name")
+) -> tuple[list, list, list, list, list, list]:
+    """Returns the data as ordered paths, so we can load the images when we need them.
+    set @show=True to plot the number of images in each dataset split."""
     X_train = sorted([x for x in data_path.glob("train/*")])
     y_train = sorted([x for x in data_path.glob("train_labels/*")])
     X_test = sorted([x for x in data_path.glob("test/*")])
@@ -20,26 +23,22 @@ def locate_data(
     y_val = sorted([x for x in data_path.glob("val_labels/*")])
 
     if show:
-        data_counts = [X_train, y_train, X_test, y_test, X_val, y_val]
+        data_counts = [X_train, X_test, X_val]
         n = len(data_counts)
-        multiplier = 0
         w = 0.5
-        x = np.arange(n)  # x positions
+        x = np.arange(n)
 
-        _, ax = plt.subplots(figsize=(6, 5), layout="constrained")
+        _, ax = plt.subplots(figsize=(4, 3), layout="constrained")
         for i in range(n):
             value = len(data_counts[i])
-            rect = ax.bar(
-                x=x[i], height=value, width=w
-            )
+            rect = ax.bar(x=x[i], height=value, width=w)
             ax.bar_label(rect, padding=3)
-            #ax.bar(x=labels, height=[len(y_train), len(y_test), len(y_val)], width=w)
-            ax.set_title("Data Distribution")
-        labels = ["Train", "Test", "Validation"]
-        #plt.xticks(x, labels)
+            ax.set_title("Dataset Split")
+        labels = ["Training", "Test", "Validation"]
+        plt.ylim(0, 420)
+        plt.xticks(x, labels)
 
     return (
-        class_lables,
         X_train,
         y_train,
         X_test,
@@ -50,5 +49,71 @@ def locate_data(
 
 
 def load_data(file_paths: list[Path]) -> np.ndarray:
-    """Load images into memory with uint8 dtype (format PyTorch likes"""
-    return np.array([np.array(Image.open(x), dtype=np.uint8) for x in file_paths])
+    """Load images into memory with uint8 dtype (dtype PyTorch prefers)"""
+    print("Loading images into memory...")
+    return_array = []
+    for i in tqdm(range(len(file_paths))):
+        return_array.append(np.array(Image.open(file_paths[i]), dtype=np.uint8))
+
+    return np.array(return_array)
+
+
+def count_classes(images: list[np.ndarray]) -> dict:
+    """Counts the number of pixels for each class in the dataset.
+    The classes are defined in @CLASS_COLOR_LABELS from _types.py module."""
+    class_counts = {c: 0 for c in CLASS_COLOR_LABELS.keys()}
+
+    for i in tqdm(images):
+        for c, v in CLASS_COLOR_LABELS.items():
+            # creates a boolean mask of current class on current image,
+            # and sums the number of pixels in the mask:
+            mask = np.all(i == v, axis=2)
+            class_pixel_count = np.sum(mask)
+
+            class_counts[c] += class_pixel_count
+
+    # pickle class_counts
+    if Path("dev_output/y_train_class_count.pkl").exists():
+        with open("dev_output/y_train_class_count.pkl", "wb") as f:
+            pickle.dump(class_counts, f)
+
+    return class_counts
+
+
+def verify_dimensions(
+    data: list[np.ndarray],
+    height: int,
+    width: int,
+) -> bool:
+    """Verify that all images in @data have the provided dimensions (@height, @width)"""
+    return all([x.shape[:-1] == (height, width) for x in data])
+
+
+def search_for_classes(classes: list[RGB]) -> list:
+    """Searches for classes in the provided data and returns the indices of the images that contain them"""
+    imageIdxs_containing_classes: list = []
+    return imageIdxs_containing_classes
+
+
+def show_imageNlabel(
+    image_idx: list[int], X_train: np.ndarray, y_train: np.ndarray
+) -> None:
+    """Plots images indexed by @image_idx from the training/test/val data.
+    Requires that images are loaded into memory in the same order (both for labels and raw)
+    """
+    n = len(image_idx)  # number of images
+    n_cols = 2
+    fig, ax = plt.subplots(nrows=n, ncols=n_cols, figsize=(12, n * 5))
+    f = 16
+
+    ax[0, 0].set_title("Original Images", fontsize=f)
+    ax[0, 1].set_title("Labeled Images", fontsize=f)
+
+    for i in range(0, n):  # for each image
+        for j in range(0, n_cols):  # for each column
+            if j == 0:
+                ax[i, j].imshow(X_train[image_idx[i]])
+            elif j == 1:
+                ax[i, j].imshow(y_train[image_idx[i]])
+
+    plt.tight_layout()
