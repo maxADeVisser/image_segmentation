34d6bfe8d8afaede4bbe2e90e229a597d80c0db0
Max de Visser <m.madv96@gmail.com>
2023-04-30 13:30:15 +0200

    updated IoU function

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,19 +1,13 @@
 import numpy as np
+import torch
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from torchmetrics.classification import MulticlassJaccardIndex
+from torchmetrics import JaccardIndex
 
 from _types import CLASS_COUNT
 
-SMOOTH = 1e-6  # To avoid division by zero
 
-
-def mIoU(y_pred: np.ndarray, y_true: np.ndarray) -> float:
-    """See: https://torchmetrics.readthedocs.io/en/stable/classification/jaccard_index.html
-    """
-    jaccard = MulticlassJaccardIndex(num_classes=CLASS_COUNT)
-    total_iou = 0
-    
-    for i in range(len(y_pred)):
-        total_iou += jaccard(y_pred[i], y_true[i])
-    
-    return total_iou / len(y_pred)
+def mIoU(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Calculates the mean intersection over union (mIoU) score.
+    See: https://torchmetrics.readthedocs.io/en/stable/classification/jaccard_index.html"""
+    jac = JaccardIndex(num_classes=CLASS_COUNT, task="multiclass")
+    return float(jac(torch.from_numpy(y_pred), torch.from_numpy(y_true)))
