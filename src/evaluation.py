768d53246c23191c2710abeb0c03e5b81d54ed6a
Max de Visser <m.madv96@gmail.com>
2023-04-30 13:30:15 +0200

    updated IoU function

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,19 +1,12 @@
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from torchmetrics.classification import MulticlassJaccardIndex
+from torchmetrics import JaccardIndex
 
 from _types import CLASS_COUNT
 
-SMOOTH = 1e-6  # To avoid division by zero
 
-
-def mIoU(y_pred: np.ndarray, y_true: np.ndarray) -> float:
+def mIoU(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """See: https://torchmetrics.readthedocs.io/en/stable/classification/jaccard_index.html
     """
-    jaccard = MulticlassJaccardIndex(num_classes=CLASS_COUNT)
-    total_iou = 0
-    
-    for i in range(len(y_pred)):
-        total_iou += jaccard(y_pred[i], y_true[i])
-    
-    return total_iou / len(y_pred)
+    jac = JaccardIndex(num_classes=CLASS_COUNT, task="multiclass")
+    return float(jac(y_pred, y_true))
